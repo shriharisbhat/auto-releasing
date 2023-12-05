8bc1fc49b06960b2327ea207d08468ac849d6da9
Shrihari Bhat <srihari.bhat@robosoftin.com>
2023-12-05 19:57:33 +0530

    feat: new feature (#9)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Save to reload</p>
+        <p>New Feature</p>
         <a
           className="App-link"
           href="https://reactjs.org"
