c139cd9d66a848bdf1c1083af1c51d382d2bb59d
sriharisbhat <srihari.bhat@robosoftin.com>
2023-12-05 20:09:50 +0530

    feat(api)!: send an email to the customer when a product is shipped

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>New Feature</p>
+        <p>Check the version upgrade</p>
         <a
           className="App-link"
           href="https://reactjs.org"
