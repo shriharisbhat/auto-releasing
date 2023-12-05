edb8ef208fed0f9501a732ef9698584cea65fdbc
Shrihari Bhat <srihari.bhat@robosoftin.com>
2023-12-05 20:14:34 +0530

    fix: check fix (#14)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Check the version upgrade</p>
+        <p>Fix Check the version upgrade</p>
         <a
           className="App-link"
           href="https://reactjs.org"
