f84aa992db1b4a1a16f6a1cf44deb7d17c178f0b
sriharisbhat <srihari.bhat@robosoftin.com>
2023-12-06 18:45:43 +0530

    chore(package): check chore release

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Fix Check the version upgrade</p>
+        <p>Fix Check chore</p>
         <a
           className="App-link"
           href="https://reactjs.org"
