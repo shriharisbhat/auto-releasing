29a95622a863981bfb7e8ef9075a4020ed2b6a69
sriharisbhat <srihari.bhat@robosoftin.com>
2023-12-08 14:34:41 +0530

    chore: check PR updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Check for autorelease tagged</p>
+        <p>Check PR updates</p>
         <a
           className="App-link"
           href="https://reactjs.org"
